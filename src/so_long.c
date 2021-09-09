ddde5eb1e7a54f49dbdfb19f2902a9842a0eef4b
Paulo Marques <paulormarques21@gmail.com>
2021-09-09 00:48:05 -0300

    Fix: correction in file extension check
    - also code refactoring

--- src/so_long.c
+++ src/so_long.c
@@ -6,47 +6,58 @@
 /*   By: proberto <proberto@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/26 12:02:34 by proberto          #+#    #+#             */
-/*   Updated: 2021/09/01 23:55:55 by proberto         ###   ########.fr       */
+/*   Updated: 2021/09/09 02:13:56 by proberto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
-static void	validate_args(int argc, char **argv);
+static void	ft_validate_args(int argc, char **argv);
 
 /**
- * @brief Main function
- * @param argc Number of arguments
+ * @brief Main function.
+ * @param argc Number of arguments.
  * @param argv Argument array, where argv[0] is the program name and
- * argv [1] is expected the name of the map file
- * @return 0 if the program ends successfully, 1 otherwise
+ * argv [1] is expected the name of the map file.
+ * @return 0 if the program ends successfully, 1 otherwise.
 */
 int	main(int argc, char **argv)
 {
-	validate_args(argc, argv);
-	if (!map_mngmt((const char **)argv))
+	ft_validate_args(argc, argv);
+	if (!ft_map_mngmt((const char **)argv))
 		exit(1);
 	return (0);
 }
 
 /**
- * @brief Validates the arguments passed to the program
- * @param argc Number of arguments
+ * @brief Validates the arguments passed to the program.
+ * @param argc Number of arguments.
  * @param argv Argument array, where argv [1] is expected the name of the
- * map file
+ * map file.
 */
-static void	validate_args(int argc, char **argv)
+static void	ft_validate_args(int argc, char **argv)
 {
+	char	*file;
+	char	*extention;
+	size_t	len_file;
+	size_t	len_ext;
+
+	file = argv[1];
+	extention = ".ber";
+	len_ext = ft_strlen(extention);
+	len_file = ft_strlen(file) - ft_strlen(extention);
 	if (argc < 2)
 	{
-		printf("Error: No arguments provided\nusage: ./so_long <map>\n");
+		ft_error("no arguments provided\n"
+			"	 usage: ./so_long <map>\n");
 		exit(1);
 	}
 	else if (argc > 2)
-		printf("Warning: too many arguments, only the first arg will be used\n");
-	if (ft_strnstr(argv[1], ".ber", ft_strlen(argv[1])) == NULL)
+		ft_warning("too many arguments, only the first arg will be used\n");
+	if (ft_strnstr(file + len_file, extention, len_ext) == NULL)
 	{
-		printf("Error: invalid file extension\nusage: ./so_long <map.ber>\n");
+		ft_error("invalid file extension\n"
+			"	 usage: ./so_long <map.ber>\n");
 		exit(1);
 	}
 }
